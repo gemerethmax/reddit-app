6640958bbfb021b39d72dde692312ebea2f40d0f
Max Gemereth <177759368+gemerethmax@users.noreply.github.com>
2025-04-30 09:57:00 -0400

    Vite installed to use Tailwind. Files renamed to include  for vite confiuration. index.html moved out of public for Vite.

--- src/index.jsx
+++ src/index.jsx
@@ -8,9 +8,11 @@ import { store } from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <>
+    <Provider store={store}>
+       <App />
+    </Provider>
+  </>
 
 );
 
